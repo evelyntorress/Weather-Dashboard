7690ff676b3e285b5249ece215bde3bb40178d61
Evelyn Torres <evelyn@MacBook-Air.hitronhub.home>
2022-07-13 16:52:17 -0700

    Adding README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# i need to be done
+# 06 Server-Side APIs: Weather Dashboard
+
+## Description
+
+This application uses Third-party APIs to access their data and functionality by making requests with specific parameters to a URL. 
+A weather dashboard will run in the browser and feature dynamically updated HTML and CSS.
+
+Use the [OpenWeather One Call API](https://openweathermap.org/api/one-call-api) to retrieve weather data for cities. Also, use `localStorage.` 
+
+When searching for a city, I am presented with the city’s current and future conditions, and that city is added to the search history.
+
+Display current weather conditions for that city, including the date, an icon representing weather conditions, the temperature, the humidity, the wind speed, and the UV index, with a color that indicates whether the conditions are favorable, moderate, or severe.
+
+Check for future weather conditions for that city. I am presented with a 5-day forecast that displays the date, an icon representing weather conditions, the temperature, the wind speed, and the humidity.
+If I click on a city in the search history, I am again presented with current and future conditions for that city.
+
+
+## Mock-Up
+
+The following image shows the web application's appearance and functionality:
+
+![The weather app ](./Assets/06-server-side-apis-homework-demo.png)
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
 
    
-// //  current day and time using moment.js
+//  current day and time using moment.js
 
 // var today = moment();
 // $("#currentDay").text(today.format("MMM Do, YYYY"));
-
-// var time = moment().format("hh:mm:ss");
-// $("#time-display").text(time);
 
 var cityName = document.getElementById("cityName");
 var citySearch = document.getElementById("citySearch");
@@ -15,34 +12,70 @@ var cityTemp = document.getElementById("cityTemp");
 var cityWind = document.getElementById("cityWind");
 var cityHumidity = document.getElementById("cityHumidity");
 var cityUVindex = document.getElementById("cityUVindex");
+var cardDeckEl = document.getElementById('card-deck');
 
-function getApi(requestUrl) {
+
+// Function to get city accordign search, temperature, wind, humidity
+function getApi(requestUrl, currentCityname) {
     fetch(requestUrl)
       .then(function (response) {
         return response.json();
     })
     .then(function (data){
+      var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,hourly&units=metric&appid=d1d93dfb4797fd38a2dfd1ffdf5b1f7a`
+      fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function(data){
         console.log(data);
-        console.log(data.name);
-        console.log(data.main);
-        console.log(data.main.temp);
-        console.log(data.main.wind);
-        console.log(data.main.humidity);
-        console.log(data.main.uvindex);
-
-        displayCity.textContent=data.name;
-        cityTemp.textContent= 'Temperature: ' + data.main.temp + ' C';
-        cityWind.textContent= 'Wind: ' + data.main.temp + ' MPH';
-        cityHumidity.textContent='Humidity: ' + data.main.humidity + '%';
-        cityUVindex.textContent='UV Index: ' + data.main.uvindex;
+        displayCity.textContent=currentCityname;
+        cityTemp.textContent= 'Temperature: ' + data.current.temp + ' °C';
+        cityWind.textContent= 'Wind: ' + data.current.temp + ' MPH';
+        cityHumidity.textContent='Humidity: ' + data.current.humidity + '%';
+        cityUVindex.textContent='UV Index: ' + data.current.uvi;  
+// for loop to get forecast for 5 days
+        for(let i = 0; i < 5; i++){
+          getForecast(data.daily[i]);
+        }
+    }) 
     })
   }
   
-
+// Search button
 citySearch.addEventListener("click", function(){
  console.log(cityName.value);
 
- getApi('https://api.openweathermap.org/data/2.5/weather?q='+cityName.value+'&units=metric&appid=d1d93dfb4797fd38a2dfd1ffdf5b1f7a')
+// Button to store the history of cities
+ const newButton = document.createElement("button");
+ newButton.innerText=cityName.value;
+ document.getElementById("newbutton").append(newButton);
+
+// Call the function to get the weather
+ getApi('https://api.openweathermap.org/data/2.5/weather?q='+cityName.value+'&units=metric&appid=d1d93dfb4797fd38a2dfd1ffdf5b1f7a' , cityName.value)
 })
+
+// function to get forecast
+function getForecast(forecast){
+  console.log(forecast);
+
+// Putting icon in the browser
+  var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+
+// Card with the info 
+  let weatherCard = `<div class="card border-primary mb-3" style="max-width: 18rem;">
+  <div class="card-header">Header</div><img src="${iconUrl}" width= "50px" alt="icon">
+  <div class="card-body text-primary">
+    <h5 class="card-title">Primary card title</h5>
+    <p class="card-text">humidity: ${forecast.humidity}.</p>
+  </div>
+</div>`
+
+console.log(cardDeckEl);
+cardDeckEl.innerHTML += weatherCard;
+
+}
+
+
 
 
